1899793ad1c97787d8b332b22a69783bd26b0bc6
YourBrainEatsYou <yourbraineatsyou@isleoflan.ch>
2021-10-18 23:50:08 +0200

    🤦‍ created reset password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./reset-password/reset-password.module').then(m => m.ResetPasswordModule)
   }
 ];
 
