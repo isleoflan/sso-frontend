81287f8980291979d05769b07bd4955599468073
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-08-16 19:08:45 +0200

    added sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {EffectsModule} from '@ngrx/effects';
-import {AbstractAuthApiService} from './api/abstract-auth-api.service';
-import {AbstractRegisterApiService} from './api/abstract-register-api.service';
-import {AbstractResetApiService} from './api/abstract-reset-api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Router } from "@angular/router";
+import { EffectsModule } from '@ngrx/effects';
+import { createErrorHandler, TraceService } from "@sentry/angular";
+import { AbstractAuthApiService } from './api/abstract-auth-api.service';
+import { AbstractRegisterApiService } from './api/abstract-register-api.service';
+import { AbstractResetApiService } from './api/abstract-reset-api.service';
 
-import {AuthApiService} from './api/auth-api.service';
-import {RegisterApiService} from './api/register-api.service';
-import {ResetApiService} from './api/reset-api.service';
+import { AuthApiService } from './api/auth-api.service';
+import { RegisterApiService } from './api/register-api.service';
+import { ResetApiService } from './api/reset-api.service';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {InitLoginComponent} from './components/init-login/init-login.component';
-import {RedirectComponent} from './components/redirect/redirect.component';
-import {httpInterceptorProviders} from './interceptors';
-import {CardModule} from './layout/card/card.module';
-import {AppStoreModule} from './store/app-store.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InitLoginComponent } from './components/init-login/init-login.component';
+import { RedirectComponent } from './components/redirect/redirect.component';
+import { httpInterceptorProviders } from './interceptors';
+import { CardModule } from './layout/card/card.module';
+import { AppStoreModule } from './store/app-store.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     RedirectComponent,
-    InitLoginComponent,
+    InitLoginComponent
   ],
   imports: [
     BrowserModule,
@@ -33,10 +35,16 @@ import {AppStoreModule} from './store/app-store.module';
     EffectsModule.forRoot([]),
   ],
   providers: [
+    {provide: ErrorHandler, useValue: createErrorHandler({showDialog: true})},
+    {provide: TraceService, deps: [Router]},
+    {
+      provide: APP_INITIALIZER, useFactory: () => () => {
+      }, deps: [TraceService], multi: true
+    },
     httpInterceptorProviders,
     {provide: AbstractAuthApiService, useClass: AuthApiService},
     {provide: AbstractRegisterApiService, useClass: RegisterApiService},
-    {provide: AbstractResetApiService, useClass: ResetApiService},
+    {provide: AbstractResetApiService, useClass: ResetApiService}
   ],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from "@sentry/tracing";
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn: "https://5c66f120cdfa4dceb4f36ceca2076b7f@o1125568.ingest.sentry.io/6655780",
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ["localhost", "https://sso.isleoflan.ch"],
+      routingInstrumentation: Sentry.routingInstrumentation
+    })
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+});
+
 
 if (environment.production) {
   enableProdMode();
