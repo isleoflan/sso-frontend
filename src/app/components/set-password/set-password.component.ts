3a98712b4b2721cdabbfe2322f3bb660fe29198e
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-08-16 18:52:20 +0200

    added validator for password confirmation

--- src/app/components/set-password/set-password.component.ts
+++ src/app/components/set-password/set-password.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { first, tap, catchError } from 'rxjs/operators';
 import { AbstractResetApiService } from '../../api/abstract-reset-api.service';
 import { ExecuteResetDto } from '../../interfaces/dto/execute-reset-dto';
+import { CustomValidatorService } from "../../services/custom-validator.service";
 import { AuthFacadeService } from "../../store/auth/auth-facade.service";
 
 @Component({
@@ -16,9 +17,10 @@ export class SetPasswordComponent implements OnInit {
   setPasswordForm = new FormGroup({
     password: new FormControl<string>('', [Validators.required]),
     passwordConfirm: new FormControl<string>('', [Validators.required])
-  });
+  }, [this.customValidatorService.passwordConfirm('password', 'passwordConfirm')]);
 
   constructor(
+    private customValidatorService: CustomValidatorService,
     private resetApiService: AbstractResetApiService,
     private authFacadeService: AuthFacadeService,
     private activatedRoute: ActivatedRoute,
@@ -43,20 +45,21 @@ export class SetPasswordComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const resetId = this.activatedRoute.snapshot.paramMap.get('hash') as string;
+    if (this.setPasswordForm.valid) {
+      const resetId = this.activatedRoute.snapshot.paramMap.get('hash') as string;
 
-    const executeResetDto: ExecuteResetDto = {
-      resetId,
-      password: this.setPasswordForm.value.password!
-    };
-    this.resetApiService.executeReset(executeResetDto).pipe(
-      first(),
-      tap((payload) => {
-        this.authFacadeService.setGlobalSessionId(payload.data.globalSessionId);
-        this.router.navigate(['/redirect', {externalUrl: payload.data.redirect}]);
-      })
-    ).subscribe();
-
+      const executeResetDto: ExecuteResetDto = {
+        resetId,
+        password: this.setPasswordForm.value.password!
+      };
+      this.resetApiService.executeReset(executeResetDto).pipe(
+        first(),
+        tap((payload) => {
+          this.authFacadeService.setGlobalSessionId(payload.data.globalSessionId);
+          this.router.navigate(['/redirect', {externalUrl: payload.data.redirect}]);
+        })
+      ).subscribe();
+    }
   }
 
 }
